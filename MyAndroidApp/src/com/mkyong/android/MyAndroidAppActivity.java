5e86bfcbb0823cd2d6791905ab94fd971bb9b2e4
PrathamR <prathamesh.karve@gmail.com>
2013-03-29 11:40:19 +0530

    Update MyAndroidAppActivity.java

--- MyAndroidApp/src/com/mkyong/android/MyAndroidAppActivity.java
+++ MyAndroidApp/src/com/mkyong/android/MyAndroidAppActivity.java
@@ -30,7 +30,7 @@ public class MyAndroidAppActivity extends Activity {
 			@Override
 			public void onClick(View arg0) {
 
-				Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.mkyong.com"));
+				Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.google.com"));
 				startActivity(browserIntent);
 
 			}
